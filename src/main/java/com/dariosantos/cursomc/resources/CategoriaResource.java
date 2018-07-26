89cb96b0e83cffe03caa7a155cf3308c76ed03dd
Dário Santos <dario.santos@redes.unb.br>
2018-07-26 14:43:58 -0300

    [ADD] repositories e services categoria

--- src/main/java/com/dariosantos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/dariosantos/cursomc/resources/CategoriaResource.java
@@ -3,29 +3,32 @@ package com.dariosantos.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.ws.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dariosantos.cursomc.domain.Categoria;
+import com.dariosantos.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService categoriaService; 
 
-	@GetMapping()
-	public List<Categoria> listar() {
+	//<?> pode ser qualquer tipo
+	@GetMapping(value="/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 
-		Categoria cat1 = new Categoria(1, "novo");
-		Categoria cat2 = new Categoria(2, "velho");
-
-		List<Categoria> lista = new ArrayList<Categoria>();
-
-		lista.add(cat1);
-		lista.add(cat2);
-
-		return lista;
-
+		Categoria categoria = categoriaService.find(id);
+		return ResponseEntity.ok().body(categoria);
+		
 	}
 }
