7b9c49557858ca5ef91d7e9e193c2012700f18a9
Dário Santos <dario.santos@redes.unb.br>
2018-07-30 10:12:56 -0300

    [ADD] criando entidades estado e cidade

--- src/main/java/com/dariosantos/cursomc/CursomcApplication.java
+++ src/main/java/com/dariosantos/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.dariosantos.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dariosantos.cursomc.domain.Categoria;
 import com.dariosantos.cursomc.domain.Produto;
+import com.dariosantos.cursomc.domain.Estado;
+import com.dariosantos.cursomc.domain.Cidade;
 import com.dariosantos.cursomc.repositories.CategoriaRepository;
+import com.dariosantos.cursomc.repositories.CidadeRepository;
+import com.dariosantos.cursomc.repositories.EstadoRepository;
 import com.dariosantos.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,12 +26,19 @@ public class CursomcApplication implements CommandLineRunner{
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		// TODO Auto-generated method stub
 		Categoria cat1 = new Categoria(null, "informatica");
 		Categoria cat2 = new Categoria(null, "escritorio");
@@ -42,9 +54,24 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "São Paulo");
+		Estado est2 = new Estado(null, "Minas Gerais");
+		
+		//associação muitos pra um já é feita no próprio construtor
+		Cidade c1 = new Cidade(null, "Uberlândia", est2);
+		Cidade c2 = new Cidade(null, "São Paulo", est1);
+		Cidade c3 = new Cidade(null, "Camplinas", est1);
+		
+		est1.getCidades().addAll(Arrays.asList(c2, c3));
+		est2.getCidades().addAll(Arrays.asList(c1));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		
 		
