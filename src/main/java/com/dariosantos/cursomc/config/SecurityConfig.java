1d3e31850ef6a6fec037d192ec2fd3bd8e5361ac
Dário Santos <dario.santos@redes.unb.br>
2018-08-07 16:42:41 -0300

    [ADD] aplicando segurança

---
+++ src/main/java/com/dariosantos/cursomc/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.dariosantos.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
+	// vetorizando uma string para dar permissões de acesso
+	private static final String[] PUCLIC_MATCHES = { "/h2-console/**", "/clientes/**" };
+
+	private static final String[] PUCLIC_MATCHES_GET = { "/categorias/**" };
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		// liberando o acesso ao banco H2 em ambiente de teste
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+
+		http.cors().and().csrf().disable();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUCLIC_MATCHES_GET).permitAll().antMatchers(PUCLIC_MATCHES)
+				.permitAll().anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	// permitindo o acesso aos endpoints por multiplas fontes
+	// quando esse metodo é instanciado ele sobreescreve o cors
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+}
