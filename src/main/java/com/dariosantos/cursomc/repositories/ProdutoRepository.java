e386472de89228ed333f00436f6e3d33104acd1e
Dário Santos <dario.santos@redes.unb.br>
2018-08-09 16:46:59 -0300

    [ADD] implementando lista de produtos

--- src/main/java/com/dariosantos/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/dariosantos/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,20 @@
 package com.dariosantos.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.dariosantos.cursomc.domain.Categoria;
 import com.dariosantos.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	
+	Page<Produto> search(String nome, List<Categoria> categorias, Pageable pageRequest);
+		
+	
+	
 }
---
+++ src/main/java/com/dariosantos/cursomc/services/ProdutoService.java
@@ -0,0 +1,41 @@
+package com.dariosantos.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.dariosantos.cursomc.domain.Categoria;
+import com.dariosantos.cursomc.domain.Produto;
+import com.dariosantos.cursomc.repositories.CategoriaRepository;
+import com.dariosantos.cursomc.repositories.ProdutoRepository;
+import com.dariosantos.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Produto find(Integer id) {
+		Optional<Produto> obj = produtoRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id));
+	}
+	
+	
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return produtoRepository.search(nome, categorias, pageRequest);
+	}
+
+}
