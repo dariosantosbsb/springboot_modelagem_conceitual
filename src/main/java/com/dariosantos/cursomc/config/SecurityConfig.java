25977a0123373262a86c45ecd1674b3bf7ba630b
Dário Santos <dario.santos@redes.unb.br>
2018-08-14 16:37:24 -0300

    [ADD] autenticando usuario e gerando token JWT

--- src/main/java/com/dariosantos/cursomc/config/SecurityConfig.java
+++ src/main/java/com/dariosantos/cursomc/config/SecurityConfig.java
@@ -7,14 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.dariosantos.cursomc.security.JWTAuthenticationFilter;
+import com.dariosantos.cursomc.security.utils.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
@@ -22,11 +27,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
 
 	// vetorizando uma string para dar permissões de acesso
-	private static final String[] PUCLIC_MATCHES = { "/h2-console/**", "/clientes/**", "/categorias/**", "/pedidos/**" , "/produtos/**" };
+	private static final String[] PUCLIC_MATCHES = { "/h2-console/**" };
 
-	private static final String[] PUCLIC_MATCHES_GET = { "/categoriasTeste/**" };
+	private static final String[] PUCLIC_MATCHES_GET = { "/categorias/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -36,11 +47,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			http.headers().frameOptions().disable();
 		}
 
-		//http.cors().and().csrf().disable();
-		http.csrf().disable();
+		http.cors().and().csrf().disable();
+		//http.csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUCLIC_MATCHES_GET).permitAll().antMatchers(PUCLIC_MATCHES)
 				.permitAll().anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCyptPasswordEncoder());
 	}
 
 	// permitindo o acesso aos endpoints por multiplas fontes
---
+++ src/main/java/com/dariosantos/cursomc/services/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.dariosantos.cursomc.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.dariosantos.cursomc.domain.Cliente;
+import com.dariosantos.cursomc.repositories.ClienteRepository;
+import com.dariosantos.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cliente = clienteRepository.findByEmail(email);
+		if(cliente == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cliente.getId(), cliente.getEmail(), cliente.getSenha(), cliente.getPerfis());
+	}
+
+}
